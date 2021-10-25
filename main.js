5918ab50bc2d4308df617ed76652b32cdc27ef8c
lou <dustyfingers01257@gmail.com>
2021-10-25 10:27:33 -0600

    added lighting and orbit controls to scene

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+// orbit controls allow us to move around the scene using our mouse
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; 
 
 import './style.css';
 
@@ -22,17 +24,46 @@ camera.position.setZ(30);
 
 // build geometry and material for torus
 const torusGeometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const torusMaterial = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+const torusMaterial = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 
 // build torus
 const torus = new THREE.Mesh(torusGeometry, torusMaterial);
 
-// add tous to scene
+// add torus to scene
 scene.add(torus);
+
+// build and configure point light
+const pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.set(20, 20, 20);
+
+// build ambient light and add to scene
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+
+// add lights to scene
+scene.add(pointLight, ambientLight);
+
+// lights helpers show where the light is in the scene using a little wireframe
+const pointLightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(pointLightHelper);
+
+// grid helpers show where the grid is in the scene
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
+
+// build controls 
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // constantly rerender
 const animate = () => {
   requestAnimationFrame(animate);
+
+  // rotates the torus
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
   renderer.render(scene, camera);
 }
 
